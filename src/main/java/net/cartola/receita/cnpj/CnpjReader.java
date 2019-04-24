f48e69288b6ddadd75a895b5aad22a90ff7e314e
Murilo Tuvani <murilo.tuvani@gmail.com>
2019-04-24 00:52:11 -0300

    Comecando ...

---
+++ src/main/java/net/cartola/receita/cnpj/CnpjReader.java
@@ -0,0 +1,65 @@
+package net.cartola.receita.cnpj;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * 24/04/2019 00:26:37
+ *
+ * @author murilo
+ */
+public class CnpjReader {
+
+    public static void main(String[] args) {
+        File f = new File("/Users/murilo/Documents/cnpj/F.K032001K.D90308");
+        if (f.exists() && f.canRead()) {
+            CnpjReader c = new CnpjReader();
+            c.read(f);
+        } else {
+            System.out.println("Nao encontrou o arquivo : " + f.getAbsolutePath());
+        }
+    }
+
+    private void read(File f) {
+        try (FileReader     fr = new FileReader(f);
+             BufferedReader br = new BufferedReader(fr)) {
+            String line = null;
+            int header = 0;
+            int detalhe = 0;
+            int cnaeSecundario = 0;
+            int socio = 0;
+            int trailler = 0;
+            int outros = 0;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("1")) {
+                    detalhe++;
+                } else if (line.startsWith("2")) {
+                    socio++;
+                } else if (line.startsWith("6")) {
+                    cnaeSecundario++;
+                } else if (line.startsWith("0")) {
+                    header++;
+                } else if (line.startsWith("9")) {
+                    trailler++;
+                } else {
+                    outros++;
+                }
+            }
+            System.out.println("Header   : " + header);
+            System.out.println("Empresa  : " + detalhe);
+            System.out.println("Sócio    : " + socio);
+            System.out.println("CNAE Sec : " + cnaeSecundario);
+            System.out.println("Trailler : " + trailler);
+            System.out.println("Outros   : " + outros);
+            System.out.println("Header   : " + header);
+        } catch (IOException ex) {
+            Logger.getLogger(CnpjReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
